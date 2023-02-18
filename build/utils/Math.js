681e1eaafd94bc6676535e56cd5d5925c8628385
WHYTEWYLL <juan@Juans-MacBook-Pro.local>
2023-02-18 14:20:35 -0300

    gbuild

--- build/utils/Math.js
+++
@@ -1,3 +0,0 @@
-export const randomeNum = (max) => { return Math.floor(Math.random() * max); };
-export const abs = (num) => { return Math.abs(num); };
-export const hypot = (x, y) => { return Math.hypot(x, y); };
--- build/utils/load_sprites.js
+++
@@ -1,5 +0,0 @@
-export const load_sprite = (sprite) => {
-    const img = new Image();
-    img.src = sprite;
-    return img;
-};
